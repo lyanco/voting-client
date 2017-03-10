a19ee30282e2ac0df92705d8cebad49a59fd2d00
Lee Yanco <lee.yanco@gmail.com>
2017-03-10 15:55:14 -0500

    Implement client-side UI with placeholder functions and data

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
+import './index.css';
+import Voting from './components/Voting';
+import Results from './components/Results';
+
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
+
+ReactDOM.render(
+  <Router history={hashHistory}>{routes}</Router>,
+  document.getElementById('root')
+);
